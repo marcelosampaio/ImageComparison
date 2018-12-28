64dcb5a1f5bbe72c350a9c656fdcb95713c83354
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-28 09:04:43 -0200

    initial commit

--- ImageComparison/Controllers/MainController.swift
+++ ImageComparison/Controllers/MainController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainController.swift
 //  ImageComparison
 //
 //  Created by Marcelo Sampaio on 28/12/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
