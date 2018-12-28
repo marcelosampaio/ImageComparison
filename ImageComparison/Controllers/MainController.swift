d538bea0f3963479bc8462bbecb373a1c2ea9a7e
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-28 09:16:11 -0200

    working on setup 🍬

--- ImageComparison/Controllers/MainController.swift
+++ ImageComparison/Controllers/MainController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class MainController: UIViewController {
+class MainController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // MARK: - Properties
+    private var source = [String]()
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var refreshButton: UIBarButtonItem!
+    @IBOutlet weak var actionButton: UIBarButtonItem!
+    
+    
+    // MARK: - Vierw Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    // mARK: - Table View DataSource and Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return source.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = source[indexPath.row]
+        return cell!
+    }
+    
 
 }
 
