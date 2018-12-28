bf044383c906bb2093d9537229f9c38d5ec42087
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-28 08:56:41 -0200

    Initial Commit

---
+++ ImageComparison/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageComparison
+//
+//  Created by Marcelo Sampaio on 28/12/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
